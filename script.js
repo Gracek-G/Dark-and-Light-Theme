4038db39a282aaf769e0f7f9f0eef6fc6eb214b0
Gracek-G <gracjan.gutowski@gmail.com>
2022-07-08 21:42:43 +0200

    Simplified the darkMode and lightMode functions by lowering the number of lines and creating an additional function imageMode to automaticly change the images. Added Contacts Web Links functions but didn't connect them to any functionality. Still needs work.

--- script.js
+++ script.js
@@ -6,16 +6,21 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
+// Dark or Light Images
+
+function imageMode(color) {
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_conceptual_idea_${color}.svg`;
+    image3.src = `img/undraw_feeling_proud_${color}.svg`;
+}
+
 // Dark Mode Styles
 function darkMode () {
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     toggleIcon.children[0].textContent = 'Dark Mode';
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[1].classList.add('fa-moon');
-    image1.src = 'img/undraw_proud_coder_dark.svg';
-    image2.src = 'img/undraw_conceptual_idea_dark.svg';
-    image3.src = 'img/undraw_feeling_proud_dark.svg';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
 }
 
 //Light Mode Styles
@@ -23,11 +28,8 @@ function lightMode () {
     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = 'Light Mode';
-    toggleIcon.children[1].classList.remove('fa-moon');
-    toggleIcon.children[1].classList.add('fa-sun');
-    image1.src = 'img/undraw_proud_coder_light.svg';
-    image2.src = 'img/undraw_conceptual_idea_light.svg';
-    image3.src = 'img/undraw_feeling_proud_light.svg';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
 }
 
 //Switch Theme Dynamically
@@ -39,6 +41,27 @@ function switchTheme(event) {
         document.documentElement.setAttribute('data-theme', 'light');
         lightMode();
     }
+}
+
+// Contacts web links
+function openTwitchLink () {
+    const twitchLink = `https://www.twitch.tv/orcangg`;
+    window.open(twitchLink, '_blank');
+}
+
+function openGitHubLink () {
+    const gitHubLink = `https://github.com/Gracek-G`;
+    window.open(gitHubLink, '_blank');
+}
+
+function openYouTubeLink () {
+    const youTubeLink = `https://www.youtube.com/watch?v=b9TE3226T-g`;
+    window.open(youTubeLink, '_blank');
+}
+
+function openTwitterLink () {
+    const twitterLink = `https://twitter.com/OrcanGG`;
+    window.open(twitterLink, '_blank');
 }
 
 //Event Listener
